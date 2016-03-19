91101e595a8a9e0cf3b6f66b8566e4c12e04b361
Anna <annachen368@gmail.com>
2016-03-19 15:58:13 -0700

    ball, wall, scene first version completed

---
+++ Assets/scripts/Camera_controller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_controller : MonoBehaviour {
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+
+    void LateUpdate() {
+        transform.position = player.transform.position + offset;
+
+    }
+    
+    
+}
---
+++ Assets/scripts/Player_controller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_controller : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+}
